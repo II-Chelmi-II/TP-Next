eb97a99ba38484cb167238bc0c699f45a1c7e283
Ramilison Michel Heather <hei.michel.8@gmail.com>
2024-04-21 17:51:22 +0300

    feat: Add content

--- app/about.tsx
+++ app/about.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
-export const about = () => {
+const About = () => {
   return (
-    <div>about</div>
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our website! We are dedicated to providing you with the best services and products.</p>
+      <p>Our mission is to...</p>
+      <ul>
+        <li>Deliver exceptional customer satisfaction</li>
+        <li>Create innovative solutions</li>
+        <li>Build strong relationships with our clients</li>
+      </ul>
+      <p>Contact us to learn more about our company and how we can assist you.</p>
+    </div>
   )
 }
+
+export default About
